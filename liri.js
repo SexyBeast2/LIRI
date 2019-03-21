fd02b2ec428ddc14f3777a9c3ba3b79d2d9dfc17
Papa Paady <paadmartin@gmail.com>
2019-03-21 00:11:39 -0700

    npm directory

--- liri.js
+++ liri.js
@@ -9,16 +9,18 @@ axios.get("http://www.omdbapi.com/?apikey=296ab7cc").then(function(response) {
     console.log("The movie's rating is: " + response.data.imdbRating);
   }
 ); 
+
+//"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
  
-var spotify = new Spotify({
-  id: <your spotify client id>,
-  secret: <your spotify client secret>
-});
+// var spotify = new Spotify({
+//   id: <your spotify client id>,
+//   secret: <your spotify client secret>
+// });
  
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+{/* spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
   if (err) {
     return console.log('Error occurred: ' + err);
   }
  
 console.log(data); 
-});
+}); */}
