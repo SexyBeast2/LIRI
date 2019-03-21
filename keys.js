97e056a996e7ea5f5dc915a21c15fa694c4847d2
Papa Paady <paadmartin@gmail.com>
2019-03-20 23:57:22 -0700

    npm installations and files added

---
+++ keys.js
@@ -0,0 +1,6 @@
+console.log('this is loaded');
+
+exports.spotify = {
+  id: process.env.79dc4eda797347cea622aba84d7fe5f9,
+  secret: process.env.aee6d43a5d5943d49fe9d9ab98874d25
+};
---
+++ liri.js
@@ -0,0 +1,24 @@
+//npm install dotenv / axios / node-spotify-api
+
+const dotenv = require('dotenv').config();
+var keys = require("./keys.js");
+const axios = require("axios");
+const spotify = require("node-spotify-api");
+
+axios.get("http://www.omdbapi.com/?apikey=296ab7cc").then(function(response) {
+    console.log("The movie's rating is: " + response.data.imdbRating);
+  }
+); 
+ 
+var spotify = new Spotify({
+  id: <your spotify client id>,
+  secret: <your spotify client secret>
+});
+ 
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+});
